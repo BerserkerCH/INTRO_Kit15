57122eb10aeee98ac5368199969eb2073fc9cc5e
Pascal Steck <pascal.m.steck@gmail.com>
2018-09-28 14:29:20 +0200

    delete all Killme + F22KFX512 projects move TEAM_* projects to repo root

--- Killme_Project/Debug/Project_Settings/Startup_Code/startup.d
+++
@@ -1,8 +0,0 @@
-Project_Settings/Startup_Code/startup.o: \
- ../Project_Settings/Startup_Code/startup.c \
- /Users/Berserker-MBA/INTRO_Kit15/Killme_Project/Generated_Code/IO_Map.h \
- /Users/Berserker-MBA/INTRO_Kit15/Killme_Project/Static_Code/IO_Map/MK22F12.h
-
-/Users/Berserker-MBA/INTRO_Kit15/Killme_Project/Generated_Code/IO_Map.h:
-
-/Users/Berserker-MBA/INTRO_Kit15/Killme_Project/Static_Code/IO_Map/MK22F12.h:
